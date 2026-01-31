90d0c081688aa606fae0efa0f9253c3ce33261da
Ashrith99 <ashrith.vutchula58@gmail.com>
2026-01-31 21:53:35 +0530

    dummy test after deleting the VITE_LIVEKIT_URL

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -9,7 +9,6 @@ const Booking = () => {
   const navigate = useNavigate();
   const [date, setDate] = useState<Date | undefined>(new Date());
 
-  
   return (
     <div className="min-h-screen bg-background">
       {/* Header */}
